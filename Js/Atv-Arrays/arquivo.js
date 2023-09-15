9e28a9e92cf668c0877f0d1df6e99bc740c84173
Reginaldo Alves <37334200+Reginaldo54@users.noreply.github.com>
2023-09-15 16:39:55 -0300

    Update arquivo.js

--- Js/Atv-Arrays/arquivo.js
+++ Js/Atv-Arrays/arquivo.js
@@ -1,17 +1,21 @@
+// Teste de uso de arquivos
+
+// importando biblioteca
 const fs = require('fs');
 
 let nomeArquivo = "arquivo.txt";
-let conteudo = "\nSim, eu num já te disse? Tu é surdo?";
+
+let conteudo = "\nSim, e você?";
+
 // Leitura do arquivo
 fs.readFile(nomeArquivo, 'utf8', (err, data) => {
     if (err) {
       console.error('Erro ao ler o arquivo:', err);
       return;
     }
-  
+    
+    //Mostrando o contéudo  
     console.log('Conteúdo do arquivo:', data);
-  
-    // Agora, você pode realizar outras operações com o conteúdo do arquivo
   
     // Fechando o arquivo (não é estritamente necessário, pois o Node.js gerencia isso automaticamente)
     fs.closeSync(fs.openSync(nomeArquivo, 'r'));
